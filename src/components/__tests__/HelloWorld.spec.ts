79154a91c37647322c1b5c81e467ed3dc4cd34c7
linkmss <59558119+linkmss@users.noreply.github.com>
2022-10-30 21:49:59 +0600

    dev. Del components

--- src/components/__tests__/HelloWorld.spec.ts
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -1,11 +1,15 @@
-import { describe, it, expect } from 'vitest'
+import { describe, expect } from 'vitest'
+//
+// import { mount } from '@vue/test-utils'
+// import HelloWorld from '../HelloWorld.vue'
+//
+// describe('HelloWorld', () => {
+//   it('renders properly', () => {
+//     const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
+//     expect(wrapper.text()).toContain('Hello Vitest')
+//   })
+// })
 
-import { mount } from '@vue/test-utils'
-import HelloWorld from '../HelloWorld.vue'
-
-describe('HelloWorld', () => {
-  it('renders properly', () => {
-    const wrapper = mount(HelloWorld, { props: { msg: 'Hello Vitest' } })
-    expect(wrapper.text()).toContain('Hello Vitest')
-  })
+describe('Test', () => {
+    expect(1).equal(1)
 })
